f25eabf2baa993de5418587e9b74d42e3b68ab71
Callum Nicholson <hamsterjam99@gmail.com>
2018-02-21 21:21:00 +0900

    Added in the exceptions for out of bounds

--- vm/source/Memory.cpp
+++ vm/source/Memory.cpp
@@ -3,6 +3,8 @@
 #include <cstdlib>
 #include <cstring>
 
+#include <stdexcept>
+
 Memory::Memory(std::size_t words) {
     this->data = (unsigned int*) malloc(sizeof(unsigned int) * words);
     this->words = words;
@@ -14,7 +16,7 @@ Memory::~Memory() {
 
 void Memory::setRange(std::size_t index, unsigned int* values, std::size_t length) {
     if (index + length >= this->words) {
-        // I can't remember how exceptions work
+        throw std::out_of_range("Memory::setRange");
     }
 
     memcpy(this->data + index, values, sizeof(unsigned int) * length);
@@ -22,7 +24,7 @@ void Memory::setRange(std::size_t index, unsigned int* values, std::size_t lengt
 
 unsigned int& Memory::operator[](std::size_t index) {
     if (index >= this->words) {
-        // I can't remember how exceptions work
+        throw std::out_of_range("Memory::operator[]");
     }
 
     return this->data[index];
--- vm/test/Memory.cpp
+++ vm/test/Memory.cpp
@@ -1,5 +1,6 @@
 #include <Memory.hpp>
 #include <iostream>
+#include <stdexcept>
 
 using namespace std;
 
@@ -10,7 +11,7 @@ int main(int argc, char** argv) {
 
     {
         // I'm just going to store the numbers 0 ~ 2^16-1 in the array as a test.
-        cout << "Testing store and recall... \t";
+        cout << "Testing store and recall... \t\t";
         for (size_t i = 0; i < words; ++i) {
             testMem[i] = i;
         }
@@ -34,7 +35,7 @@ int main(int argc, char** argv) {
 
     {
         // Test setRange
-        cout << "Testing setRange... \t\t";
+        cout << "Testing setRange... \t\t\t";
         unsigned int data[] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 26};
         testMem.setRange(0x1337, data, 10);
 
@@ -44,6 +45,26 @@ int main(int argc, char** argv) {
                 pass = false;
                 break;
             }
+        }
+
+        if (pass) {
+            cout << "OK!" << endl;
+        }
+        else {
+            cout << "Fail" << endl;
+        }
+    }
+
+    {
+        // Test out of range indexing
+        cout << "Testing out of range indexes... \t";
+
+        bool pass = false;
+        try {
+            testMem[1 << 16] = 0;
+        }
+        catch(std::out_of_range e) {
+            pass = true;
         }
 
         if (pass) {
