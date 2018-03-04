811c1052c904d6488c760e76b41a38415f101b34
Callum Nicholson <hamsterjam99@gmail.com>
2018-03-05 08:08:46 +0900

    Made input to hex2bin more robust
    Now it ignores empty lines and comments begining with #

--- hex2bin/hex2bin.cpp
+++ hex2bin/hex2bin.cpp
@@ -1,11 +1,22 @@
 // Reads a hex string from stdin and outputs binary on stdout
 #include <iostream>
+#include <limits>
+
+const std::streamsize maxStreamSize = std::numeric_limits<std::streamsize>::max();
 
 int main(int argc, char** argv) {
     char buff[3];
     buff[2] = 0;
     while (std::cin.peek() != std::istream::traits_type::eof()) {
         std::cin >> std::ws;
+        if (std::cin.peek() == '\n') {
+            std::cin.get();
+            continue;
+        }
+        if (std::cin.peek() == '#') {
+            std::cin.ignore(maxStreamSize, '\n');
+            continue;
+        }
         std::cin.read(buff, 2);
         std::cout.put((char) std::stoul(buff, NULL, 16));
     }
