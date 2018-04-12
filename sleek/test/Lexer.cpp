65eaf57237688a2cf4c3d60107f6fb417c219363
Callum Nicholson <hamsterjam99@gmail.com>
2018-04-12 23:32:30 +0900

    Added short token names

--- sleek/test/Lexer.cpp
+++ sleek/test/Lexer.cpp
@@ -9,8 +9,36 @@
 
 #define SS_EOF (std::stringstream::traits_type::eof())
 
-#define t(x) (Token::Type::x)
 #define assert(x) (pass = pass && (x))
+
+// Short token names
+const Token::Type ID     = Token::Type::IDENTIFIER;
+const Token::Type KEY    = Token::Type::KEYWORD;
+const Token::Type INT    = Token::Type::INTEGER;
+const Token::Type OB     = Token::Type::OPENING_BLOCK;
+const Token::Type CB     = Token::Type::CLOSING_BLOCK;
+const Token::Type OP     = Token::Type::OPENING_PAREN;
+const Token::Type CP     = Token::Type::CLOSING_PAREN;
+const Token::Type OAS    = Token::Type::OPENING_ACCESS_SPECIFIER;
+const Token::Type CAS    = Token::Type::CLOSING_ACCESS_SPECIFIER;
+const Token::Type OIB    = Token::Type::OPENING_INDEX_BRACKET;
+const Token::Type CIB    = Token::Type::CLOSING_INDEX_BRACKET;
+const Token::Type OAL    = Token::Type::OPENING_ARG_LIST;
+const Token::Type CAL    = Token::Type::CLOSING_ARG_LIST;
+const Token::Type OAL_CT = Token::Type::OPENING_ARG_LIST_CT;
+const Token::Type CAL_CT = Token::Type::CLOSING_ARG_LIST_CT;
+const Token::Type OPL    = Token::Type::OPENING_PARAM_LIST;
+const Token::Type CPL    = Token::Type::CLOSING_PARAM_LIST;
+const Token::Type OPL_CT = Token::Type::OPENING_PARAM_LIST_CT;
+const Token::Type CPL_CT = Token::Type::CLOSING_PARAM_LIST_CT;
+const Token::Type PA     = Token::Type::PROPERTY_ACCESS;
+const Token::Type COM    = Token::Type::COMMA;
+const Token::Type DEF    = Token::Type::DEFINITION;
+const Token::Type OVER   = Token::Type::OVERLOAD;
+const Token::Type OP1    = Token::Type::UNARY_OPERATOR;
+const Token::Type OP2    = Token::Type::BINARY_OPERATOR;
+const Token::Type EOS    = Token::Type::END_OF_STATEMENT;
+const Token::Type END    = Token::Type::END_OF_FILE;
 
 class LexerTest : public Lexer {
     public:
@@ -48,10 +76,10 @@ int main(int argc, char** argv) {
         // Assert the token stream is correct
         LexerTest lex(std::move(source), sym, err);
         assert(lex.matches({
-            t(IDENTIFIER), t(DEFINITION), t(KEYWORD), t(BINARY_OPERATOR), t(INTEGER), t(END_OF_STATEMENT),
-            t(IDENTIFIER), t(DEFINITION), t(BINARY_OPERATOR), t(INTEGER), t(END_OF_STATEMENT),
-            t(IDENTIFIER), t(DEFINITION), t(KEYWORD), t(END_OF_STATEMENT),
-            t(END_OF_FILE)
+            ID, DEF, KEY, OP2, INT, EOS,
+            ID, DEF, OP2, INT, EOS,
+            ID, DEF, KEY, EOS,
+            END
         }));
 
         // Assert that there were no errors
